88f88d893153af84e00fedd54a8a131f1ac47ca5
tuvshinot <ronin.xe@gmail.com>
2020-04-24 22:16:18 +0300

    Threading - IO BOUND TASK

---
+++ threading/thread1.py
@@ -0,0 +1,44 @@
+# Example 1
+import threading
+import time
+
+start = time.perf_counter()
+
+def do_something(second=1):
+    print(f'Sleeping {second} sec')
+    time.sleep(second)
+    print('Done sleeping')
+
+t1 = threading.Thread(target=do_something)
+t2 = threading.Thread(target=do_something)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join() # Waits until two threads finish go to next line
+
+finish = time.perf_counter()
+
+print(f'Example 1: Finished in {round(finish - start, 2)} seconds')
+
+# -----------------------------------------------------------------
+# Example 2 - run 1- seconds func to 1 second
+
+start = time.perf_counter()
+
+threads = []
+
+for _ in range(10):
+    t = threading.Thread(target=do_something, args=[1.5]) # args to do something func
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
+
+finish = time.perf_counter()
+
+print(f'Exaple 2 : Finished in {round(finish - start, 2)} seconds')
+
+
---
+++ threading/thread2.py
@@ -0,0 +1,57 @@
+# Example 3
+import concurrent.futures
+import time
+
+start = time.perf_counter()
+
+def do_something(second=1):
+    print(f'Sleeping {second} sec')
+    time.sleep(second)
+    return f'Done sleeping {second}'
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    f1 = executor.submit(do_something, 1.3) # second=1.3
+    f2 = executor.submit(do_something, 1.3) # second=1.3
+    print(f1.result())
+    print(f2.result())
+
+
+finish = time.perf_counter()
+
+print(f'Exaple 3 : Finished in {round(finish - start, 2)} seconds')
+print('-----------------------------------------------------------')
+
+# ----------------------------------------------------------------
+# Example 4 running 10 times, first to finish comes out first
+
+start = time.perf_counter()
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = [executor.submit(do_something, i) for i in range(5, 1, -1)]
+
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
+
+
+finish = time.perf_counter()
+
+print(f'Exaple 4 : Finished in {round(finish - start, 2)} seconds')
+print('-----------------------------------------------------------')
+
+# ----------------------------------------------------------------
+# Example 5, finishes order that they started, first finishes 5. because 5 was called first
+
+start = time.perf_counter()
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = executor.map(do_something, range(5, 1, -1))
+    
+    for result in results:
+        print(result)
+
+finish = time.perf_counter()
+
+print(f'Exaple 5 : Finished in {round(finish - start, 2)} seconds')
+print('-----------------------------------------------------------')
